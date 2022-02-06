12fe92057ec6f495cc44cb041f4acc2b45e0dc54
edward <edwardharks@gmail.com>
2022-02-06 15:58:16 +0000

    Adds more tests

--- generator/src/test/java/com/edwardharker/fixturegenerator/FixtureProcessorTest.kt
+++ generator/src/test/java/com/edwardharker/fixturegenerator/FixtureProcessorTest.kt
@@ -47,6 +47,181 @@ public object ExampleClassFixtures {
     }
 
     @Test
+    fun `nullable types are null`() {
+        val compilation = prepareCompilation(
+            kotlin(
+                "Example.kt",
+                """
+package test
+import com.edwardharker.fixturegenerator.Fixture
+@Fixture
+class ExampleClass(val nullable: String?)
+                """.trimIndent()
+            )
+        )
+
+        val result = compilation.compile()
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+        val generatedFileText = File(compilation.kspSourcesDir, "kotlin/test/ExampleClassFixtures.kt")
+            .readText()
+
+        assertThat(generatedFileText).isEqualToKotlin(
+            """
+package test
+
+public object ExampleClassFixtures {
+  public fun exampleClass(): ExampleClass = test.ExampleClass(
+      nullable = null,
+  )
+}
+
+        """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `primitive types are default`() {
+        val compilation = prepareCompilation(
+            kotlin(
+                "Example.kt",
+                """
+package test
+import com.edwardharker.fixturegenerator.Fixture
+@Fixture
+class ExampleClass(
+    val string: String,
+    val char: Char,
+    val int: Int,
+    val float: Float,
+    val double: Double,
+    val short: Short,
+    val long: Long,
+    val byte: Byte,
+    val boolean: Boolean,
+)
+                """.trimIndent()
+            )
+        )
+
+        val result = compilation.compile()
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+        val generatedFileText = File(compilation.kspSourcesDir, "kotlin/test/ExampleClassFixtures.kt")
+            .readText()
+
+        assertThat(generatedFileText).isEqualToKotlin(
+            """
+package test
+
+public object ExampleClassFixtures {
+  public fun exampleClass(): ExampleClass = test.ExampleClass(
+      string = "",
+      char = '\u0000',
+      int = 0,
+      float = 0.0F,
+      double = 0.0,
+      short = 0,
+      long = 0L,
+      byte = 0,
+      boolean = false,
+  )
+}
+
+        """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `collection types are empty`() {
+        val compilation = prepareCompilation(
+            kotlin(
+                "Example.kt",
+                """
+package test
+import com.edwardharker.fixturegenerator.Fixture
+@Fixture
+class ExampleClass(
+    val list: List<String>,
+    val map: Map<String, String>,
+    val set: Set<String>,
+)
+                """.trimIndent()
+            )
+        )
+
+        val result = compilation.compile()
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+        val generatedFileText = File(compilation.kspSourcesDir, "kotlin/test/ExampleClassFixtures.kt")
+            .readText()
+
+        assertThat(generatedFileText).isEqualToKotlin(
+            """
+package test
+
+public object ExampleClassFixtures {
+  public fun exampleClass(): ExampleClass = test.ExampleClass(
+      list = emptyList(),
+      map = emptyMap(),
+      set = emptySet(),
+  )
+}
+
+        """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `array types are empty`() {
+        val compilation = prepareCompilation(
+            kotlin(
+                "Example.kt",
+                """
+package test
+import com.edwardharker.fixturegenerator.Fixture
+@Fixture
+class ExampleClass(
+    val array: Array<String>,
+    val intArray: IntArray,
+    val charArray: CharArray,
+    val floatArray: FloatArray,
+    val doubleArray: DoubleArray,
+    val shortArray: ShortArray,
+    val longArray: LongArray,
+    val byteArray: ByteArray,
+    val booleanArray: BooleanArray,
+)
+                """.trimIndent()
+            )
+        )
+
+        val result = compilation.compile()
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+        val generatedFileText = File(compilation.kspSourcesDir, "kotlin/test/ExampleClassFixtures.kt")
+            .readText()
+
+        println(generatedFileText)
+        assertThat(generatedFileText).isEqualToKotlin(
+            """
+package test
+
+public object ExampleClassFixtures {
+  public fun exampleClass(): ExampleClass = test.ExampleClass(
+      array = emptyArray(),
+      intArray = intArrayOf(),
+      charArray = charArrayOf(),
+      floatArray = floatArrayOf(),
+      doubleArray = doubleArrayOf(),
+      shortArray = shortArrayOf(),
+      longArray = longArrayOf(),
+      byteArray = byteArrayOf(),
+      booleanArray = booleanArrayOf(),
+  )
+}
+
+        """.trimIndent()
+        )
+    }
+
+    @Test
     fun `picks first enum value`() {
         val compilation = prepareCompilation(
             kotlin(
