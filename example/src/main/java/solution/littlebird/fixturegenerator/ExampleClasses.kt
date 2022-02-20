67e1eceb12463ac1017300ebcd8125f511c09970
edward <edwardharks@gmail.com>
2022-02-20 15:51:03 +0000

    Removes todo list from the code
    Everything is tracked in github issues now.

--- example/src/main/java/solution/littlebird/fixturegenerator/ExampleClasses.kt
+++ example/src/main/java/solution/littlebird/fixturegenerator/ExampleClasses.kt
@@ -86,36 +86,3 @@ enum class EnumExample {
 
 @Fixture
 object ExampleObject
-
-// TODO
-
-/* Inner class Fixture should be child of outer class fixture to avoid conflicts */
-//@Fixture
-//class OuterClass(
-//    val inner: InnerClass
-//) {
-//    @Fixture
-//    class InnerClass(foo: String)
-//}
-
-/* Sealed classes - how do we pick the subclass to create? */
-
-/* generate the code in the test sources */
-
-/* Generics */
-
-/* Support types that cannot be annotated */
-//@Fixture
-//data class LibraryTypes(
-//    val libraryType: Date
-//)
-
-/* How do we know which constructor to generate the fixture for?
-   Easy solution is just to use the primary constructor */
-//@Fixture
-//class MultipleConstructors(
-//    val one: Int,
-//    val two: Int,
-//) {
-//    constructor() : this(1, 2)
-//}
